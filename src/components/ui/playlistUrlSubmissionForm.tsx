75ca6d025103d0db404b419791ae966dbedcb141
Azhar Iqbal <azhariqbal2942@gmail.com>
2024-04-09 03:36:13 +0500

    Update playlistUrlSubmissionForm component and import statements

--- src/components/ui/playlistUrlSubmissionForm.tsx
+++ src/components/ui/playlistUrlSubmissionForm.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 function PlaylistUrlSubmissionForm() {
+  const [url, setUrl] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // Handle the url submission here
+  }
+
   return (
-    <div className='text-white'>playlistUrlSubmissionForm</div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="playlistUrl" className='text-white'>Playlist URL:</label>
+      <input 
+        type="text" 
+        id="playlistUrl" 
+        value={url} 
+        onChange={(e) => setUrl(e.target.value)} 
+      />
+  <button className='bg-[#1FDF64]'>
+    Submit Url
+  </button>
+    </form>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Carousel from "@/components/ui/Carousel";
+
 import PlaylistUrlSubmissionForm from "@/components/ui/playlistUrlSubmissionForm";
+import Carousel from "@/components/ui/carousel";
 
 
 
- 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-        <Carousel/>
+      <Carousel />
 
-        <PlaylistUrlSubmissionForm/>
+      <PlaylistUrlSubmissionForm />
     </div>
   );
 }
